a1b76a506d7fab04622b075d3fe65251a2b53a2b
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2024-10-13 16:27:11 +0700

    eh

--- lib/parade.ex
+++ lib/parade.ex
@@ -1,18 +1,92 @@
 defmodule Parade do
   @moduledoc """
-  Documentation for `Parade`.
+  Helpers for building Ecto queries for ParadeDB.
   """
 
   @doc """
-  Hello world.
+  Builds a ParadeDB search query that can be used in `Ecto.Query.from`
 
-  ## Examples
-
-      iex> Parade.hello()
-      :world
+      from s in Parade.search("calls_search_idx", "transcript:walking")
 
   """
-  def hello do
-    :world
+  def search(index, query)
+
+  def search(index, query) when is_binary(query) do
+    %Ecto.Query{
+      from: %Ecto.Query.FromExpr{
+        source:
+          {:fragment, [],
+           [
+             {:raw, ""},
+             {:expr, {:literal, [], [index]}},
+             {:raw, ".search("},
+             {:expr, {:^, [], [0]}},
+             {:raw, ")"}
+           ]},
+        params: [{query, :any}]
+      }
+    }
   end
+
+  def search(index, query) when is_list(query) do
+    {insides, params} =
+      build_insides(query, _acc = [], _idx = 0, _params = [])
+
+    search =
+      [
+        raw: "",
+        expr: {:literal, [], [index]},
+        raw: ".search(query => paradedb.boolean(must => ARRAY["
+      ] ++
+        insides ++
+        [raw: "]))"]
+
+    search = merge_fragments(search)
+
+    %Ecto.Query{
+      from: %Ecto.Query.FromExpr{
+        source: {:fragment, [], search},
+        params: params
+      }
+    }
+  end
+
+  defp build_insides([op | rest], acc, idx, params) do
+    case op do
+      {:parse, value} when is_binary(value) ->
+        q = [{:raw, "paradedb.parse("}, {:expr, {:^, [], [idx]}}, {:raw, ")"}]
+        build_insides(rest, [q | acc], idx + 1, [{value, :any} | params])
+
+      {:range, %{field: field, range: range}} when is_binary(field) and is_binary(range) ->
+        q = [
+          {:raw, "paradedb.range(field => "},
+          {:expr, {:^, [], [idx]}},
+          {:raw, ", range => "},
+          {:expr, {:^, [], [idx + 1]}},
+          {:raw, "::int4range)"}
+        ]
+
+        build_insides(rest, [q | acc], idx + 2, [{range, :any}, {field, :any} | params])
+    end
+  end
+
+  defp build_insides([], acc, _idx, params) do
+    {
+      acc
+      |> :lists.reverse()
+      |> Enum.intersperse({:raw, ","})
+      |> :lists.flatten(),
+      :lists.reverse(params)
+    }
+  end
+
+  defp merge_fragments([{:raw, raw1}, {:raw, raw2} | rest]) do
+    merge_fragments([{:raw, raw1 <> raw2} | rest])
+  end
+
+  defp merge_fragments([{:raw, _raw} = raw, {:expr, _expr} = expr | rest]) do
+    [raw, expr | merge_fragments(rest)]
+  end
+
+  defp merge_fragments([{:raw, _raw}] = done), do: done
 end
--- test/parade_test.exs
+++ test/parade_test.exs
@@ -1,8 +1,50 @@
 defmodule ParadeTest do
   use ExUnit.Case
-  doctest Parade
+  import Ecto.Query
 
-  test "greets the world" do
-    assert Parade.hello() == :world
+  defp all(query) do
+    {query, _cast_params, _dump_params} =
+      Ecto.Adapter.Queryable.plan_query(:all, Ecto.Adapters.Postgres, query)
+
+    query
+    |> Ecto.Adapters.Postgres.Connection.all()
+    |> IO.iodata_to_binary()
+  end
+
+  # queries from https://github.com/Moosieus/paradedb-elixir-api-proposal#the-from-expression
+  test "it sqls" do
+    assert all(
+             from(s in Parade.search("calls_search_idx", "transcript:walking"),
+               where: fragment("call_length") > 3,
+               select: s.id
+             )
+           ) ==
+             """
+             SELECT f0."id" \
+             FROM "calls_search_idx".search($1) AS f0 \
+             WHERE (call_length > 3)\
+             """
+
+    assert all(
+             from(
+               s in Parade.search("calls_search_idx",
+                 parse: "transcript:walking",
+                 range: %{field: "call_length", range: "[3,)"}
+               ),
+               join: t in "talk_groups",
+               on: t.id == s.talk_group_id,
+               select: s.id
+             )
+           ) ==
+             """
+             SELECT f0."id" \
+             FROM "calls_search_idx".search(query => \
+             paradedb.boolean(must => ARRAY[\
+             paradedb.parse($1),\
+             paradedb.range(field => $2, range => $3::int4range)\
+             ])) AS f0 \
+             INNER JOIN "talk_groups" AS t1 \
+             ON t1."id" = f0."talk_group_id"\
+             """
   end
 end
